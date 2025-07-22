680c09780f8f3b65b04aa1b26ea519c656a37356
D-Astudillo-ASC <dma3@williams.edu>
2025-07-22 13:58:26 -0400

    Removing critter and animate-pulse from logo.

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,7 +2,7 @@
 
 export default function Logo() {
   return (
-    <div className="w-16 h-16 animate-pulse">
+    <div className="w-16 h-16">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 100 100"
